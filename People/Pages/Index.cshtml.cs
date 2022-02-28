ea3004f435aa958b21af44c8604c17ad0e8a369c
Oybek Babaev <oybekbabaev15@yahoo.com>
2022-02-28 22:00:40 +0500

    Slight updates (Index, Signin, Signup)

--- People/Pages/Index.cshtml.cs
+++ People/Pages/Index.cshtml.cs
@@ -5,10 +5,14 @@ namespace People.Pages
     public class IndexModel : PageModel
     {
         public IEnumerable<string> Locations { get; set; }
+        public int CVCount { get; set; }
 
         public void OnGet()
         {
             Locations = new[] { "Tashkent, UZB", "Namangan, UZB", "Andijan, UZB" };
+
+            Random gg = new();
+            CVCount = gg.Next(50_000, 200_000);
         }
     }
 }
