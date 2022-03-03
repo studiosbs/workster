06374a729e156648f07e2035a7673355c193ac15
yusupovbg <b.yusupoff001@gmail.com>
2022-03-03 22:13:24 +0500

    Initial update

--- People/wwwroot/js/load.js
+++ People/wwwroot/js/load.js
@@ -17,26 +17,46 @@ function blurring(){
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 } */
-function onReady(callback) {
-    function loadingScreen() {
-        if (document.getElementsByTagName('body')[0] !== undefined) {
-          window.clearInterval(intervalId);
-          callback.call(this);
-        }
-        document.querySelector(".loading").style.opacity = scale(intervalId, 0, 100, 1, 0)
-      }
-    var intervalId = window.setInterval(loadingScreen, 1000);
-  }
+// const wait = (delay = 0) =>
+//   new Promise(resolve => setTimeout(resolve, delay));
+
+  
+//   function setVisible(selector, visible) {
+//     document.querySelector(selector).style.display = visible ? 'block' : 'none';
+//   }
+// /*   
+//   setVisible('.page', false);
+//   setVisible('#load', true); */
+  
+// document.addEventListener('DOMContentLoaded', wait(2000).then(() => {
+//     setVisible('#page', true);
+//     setVisible('#load', false);
+//   }));
+  
+/*   onReady(function() {
+    setVisible('#page', true);
+    setVisible('#load', false);
+  }); */
+  
+  const wait = (delay = 0) =>
+  new Promise(resolve => setTimeout(resolve, delay));
   
   function setVisible(selector, visible) {
-    document.querySelector(selector).style.display = visible ? 'block' : 'none';
+        document.querySelector(selector).style.display = visible ? 'block' : 'none';
   }
   
-  onReady(function() {
-    setVisible('.bg', true);
-    setVisible('#load', false);
-  });
-  
-  const scale = (num, in_min, in_max, out_min, out_max) => {
-    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  function work() {
+    wait(1000).then(() => {
+      setVisible('#page', true);
+      setVisible('#loading', false);
+    })
+  }
+
+  if (document.readyState == 'loading') {
+    // ещё загружается, ждём события
+    document.addEventListener('DOMContentLoaded', work);
+  } else {
+    // DOM готов!
+    setVisible('#page', false);
+    setVisible('#load', true);
   }
