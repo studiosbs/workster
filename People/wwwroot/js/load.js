eeb8c91a48b0d150a86a63b3247ed2d9d8861636
yusupovbg <b.yusupoff001@gmail.com>
2022-03-03 22:39:28 +0500

    Final update of loader screen*

--- People/wwwroot/js/load.js
+++ People/wwwroot/js/load.js
@@ -1,3 +1,7 @@
+/* window.onload(function () {
+  document.querySelector('#page').style.display="none";
+  document.querySelector('#load').style.display="block";
+})
 function onReady(callback) {
   var intervalId = window.setInterval(function() {
     if (document.getElementsByTagName('body')[0] !== undefined) {
@@ -5,34 +9,15 @@ function onReady(callback) {
       callback.call(this);
     }
   }, 2000);
-}
-
-function setVisible(selector, visible) {
-  document.querySelector(selector).style.display = visible ? 'block' : 'none';
-}
-
-onReady(function() {
-  setVisible('#page', true);
-  setVisible('#load', false);
-});
-
-
+} */
+ 
 // Fix this code
 
-/* const wait = (delay = 0) =>
+const wait = (delay = 0) =>
   new Promise(resolve => setTimeout(resolve, delay));
-
-const setVisible = (elementOrSelector, visible) => 
-  (typeof elementOrSelector === 'string'
-    ? document.querySelector(elementOrSelector)
-    : elementOrSelector
-  ).style.display = visible ? 'block' : 'none';
-
-setVisible('.page', false);
-setVisible('#loading', true);
 
 document.addEventListener('DOMContentLoaded', () =>
   wait(1000).then(() => {
-    setVisible('.page', true);
-    setVisible('#loading', false);
-  })); */
+    document.querySelector('#page').style.display="block";
+    document.querySelector('#load').style.display="none";
+  }));
